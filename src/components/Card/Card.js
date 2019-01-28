d56f2285b7f80101d74dfed08066f64a931fdc6d
Whitney Burton <whitneyburton9@gmail.com>
2019-01-27 20:06:20 -0700

    Add styling and ul to residents cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,42 +4,58 @@ import PropTypes from 'prop-types';
 
 export const Card = ({ card }) => {
   let cardType;
+  let cardTitle = card.name.toUpperCase();
+
   switch (card.type) {
     case 'person':
-      cardType = <div className='Person-card Card'>
-        <h3>{card.name}</h3>
-        <p>Species: {card.species}</p>
-        <p>Language: {card.language}</p>
-        <p>Homeworld: {card.homeworld}</p>
-        <p>Population: {card.population}</p>
-      </div>
+      cardType =
+        <div className='Person-card Card'>
+          <div className='card-title'>
+            <h3>{cardTitle}</h3>
+          </div>
+          <p><span className='info'>Species:</span> {card.species}</p>
+          <p><span className='info'>Language:</span> {card.language}</p>
+          <p><span className='info'>Homeworld:</span> {card.homeworld}</p>
+          <p><span className='info'>Population:</span> {card.population}</p>
+        </div>
       break;
     case 'planet':
-      cardType = <div className='Planet-card Card'>
-        <h3>{card.name}</h3>
-        <p>Terrain: {card.terrain}</p>
-        <p>Population: {card.population}</p>
-        <p>Climate: {card.climate}</p>
-        <p>Residents: {card.residents}</p>
-      </div>
+      cardType =
+        <div className='Planet-card Card'>
+          <div className='card-title'>
+            <h3>{cardTitle}</h3>
+          </div>
+          <p><span className='info'>Terrain:</span> {card.terrain}</p>
+          <p><span className='info'>Population:</span> {card.population}</p>
+          <p><span className='info'>Climate:</span> {card.climate}</p>
+          <span className='info'>Residents:</span>
+          <ul>
+            {card.residents.map(resident => {
+              return <li>{resident}</li>
+            })}
+          </ul>
+        </div>
       break;
     case 'vehicle':
-      cardType = <div className='Vehicle-card Card'>
-        <h3>{card.name}</h3>
-        <p>Model: {card.model}</p>
-        <p>Vehicle Class: {card.class}</p>
-        <p>Number of Passengers: {card.passengers}</p>
-      </div>
+      cardType =
+        <div className='Vehicle-card Card'>
+          <div className='card-title'>
+            <h3>{cardTitle}</h3>
+          </div>
+          <p><span className='info'>Model:</span> {card.model}</p>
+          <p><span className='info'>Vehicle Class:</span> {card.class}</p>
+          <p><span className='info'>Passengers:</span> {card.passengers}</p>
+        </div>
       break;
-    default: console.log('Sorry, no cards were found for that category.');
-  }
+    default: alert('Sorry, no cards were found for that category.');
+  };
 
   return (
     <div>
       {cardType}
     </div>
   )
-}
+};
 
 Card.propTypes = {
   card: PropTypes.object.isRequired
